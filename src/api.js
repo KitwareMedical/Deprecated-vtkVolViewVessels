cf431bdf74e4fd3bf2422a797960cec0b1c3f220
Forrest Li <forrest.li@kitware.com>
2017-11-13 23:48:45 -0500

    Add api.js, which I forgot to add

---
+++ src/api.js
@@ -0,0 +1,75 @@
+import vtkDataArray from 'vtk.js/Sources/Common/Core/DataArray';
+import vtkImageData from 'vtk.js/Sources/Common/DataModel/ImageData';
+
+export default class Api {
+  constructor(dataManager) {
+    this.dataManager = dataManager;
+    this.listeners = {};
+
+    // subscribe to server events
+    this.subscription = this.dataManager.ITKTube.onTubeGeneratorChange((item) => {
+      // TODO figure out why remote sends as array
+      let tubeItem = item;
+      if (tubeItem instanceof Array) {
+        tubeItem = tubeItem[0];
+      }
+
+      this.emit('segment', tubeItem);
+    });
+  }
+
+  addEventListener(name, func) {
+    this.listeners[name] = this.listeners[name] || [];
+    this.listeners[name].push(func);
+    // disconnect method
+    return () => {
+      this.listeners.splice(this.listeners.indexOf(func), 1);
+    };
+  }
+
+  emit(name, ...args) {
+    (this.listeners[name] || []).forEach(func => func(...args));
+  }
+
+  loadImage() {
+    return new Promise((resolve, reject) => {
+      this.dataManager.ITKTube.getVolumeData()
+        .then((dataDescription) => {
+          const reader = new FileReader();
+          reader.readAsArrayBuffer(dataDescription.scalars);
+
+          reader.addEventListener('loadend', () => {
+            const values = new window[dataDescription.typedArray](reader.result);
+            const dataArray = vtkDataArray.newInstance({ name: 'Scalars', values });
+            delete dataDescription.scalars;
+            delete dataDescription.typedArray;
+            const imageData = vtkImageData.newInstance(dataDescription);
+            imageData.getPointData().setScalars(dataArray);
+
+            resolve(imageData);
+          });
+        })
+        .catch(error => reject(error));
+    });
+  }
+
+  loadTubes() {
+    return this.dataManager.ITKTube.getTubes();
+  }
+
+  segmentTube(coords, scale) {
+    return this.dataManager.ITKTube.generateTube(...coords, scale);
+  }
+
+  setTubeColor(id, color) {
+    return this.dataManager.ITKTube.setTubeColor(id, color);
+  }
+
+  reparentTubes(parent, children) {
+    return this.dataManager.ITKTube.reparentTubes(parent, children);
+  }
+
+  deleteTube(id) {
+    return this.dataManager.ITKTube.deleteTube(id);
+  }
+}
