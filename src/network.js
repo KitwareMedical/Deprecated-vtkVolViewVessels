0899893786df6914d3bac288039c12c2a3b624a7
Forrest Li <forrest.li@kitware.com>
2017-11-28 15:39:09 -0500

    ENH: Retry ws connection if connection refused
    The server is expected to run when the client starts up, so keep
    retrying until the connection is made.

--- src/network.js
+++ src/network.js
@@ -19,10 +19,34 @@ const customProtocols = {
   }),
 };
 
+// This promise is resolved from outside the promise body, since
+// we need to notify whenever the connection is established.
+let notifyConnect = null;
+const waitForConnect = new Promise((resolve, reject) => {
+  notifyConnect = resolve;
+});
+
+// We create a dummy ITKTube protocol here so we can determine
+// which methods are actually defined when checking in the proxy.
+const proxyClient = {
+  ITKTube:
+    new Proxy(customProtocols.ITKTube(), {
+      get: (target, name) => {
+        if (!target[name]) {
+          return undefined;
+        }
+        return (...args) =>
+          waitForConnect.then(
+              () => client.ITKTube[name](...args));
+      },
+    }),
+};
+
 function start(conn) {
   connection = conn;
   client = createClient(conn, [], customProtocols);
 
+  notifyConnect();
   if (readyCallback) {
     readyCallback();
   }
@@ -38,11 +62,17 @@ function exit(timeout = 60) {
 function connect(config = {}) {
   smartConnect = SmartConnect.newInstance({ config });
   smartConnect.onConnectionReady(start);
-  smartConnect.connect();
+
+  const scheduleConnect = () => {
+    // Don't need exponential back-off for local connection
+    setTimeout(() => smartConnect.connect(), 500);
+  };
+  smartConnect.onConnectionError(scheduleConnect);
+  scheduleConnect();
 }
 
 function getClient() {
-  return client;
+  return proxyClient;
 }
 
 function getConnection() {
